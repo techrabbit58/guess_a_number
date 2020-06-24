000427e77bd2a4d2ff7b8cc0d4d2750ce428bc36
techrabbit58 <dzb@pruefbit.de>
2020-06-24 13:34:24 +0200

    gueass_a_number.py: codemaker

--- main/guess_a_number.py
+++ main/guess_a_number.py
@@ -8,7 +8,9 @@ import json
 import sys
 from cmd import Cmd
 from itertools import product, permutations
+from collections import Counter
 from random import choice
+from typing import Iterable, Tuple
 
 
 class SuperHirn(Cmd):
@@ -17,7 +19,11 @@ class SuperHirn(Cmd):
     well known code-breaking game.
     """
     prompt = '(Mastermind) '
+    
+    STOP, CONTINUE = True, False
+
     session_mode = None
+    
     settings = {
         'colors': 8,
         'pins': 4,
@@ -26,11 +32,12 @@ class SuperHirn(Cmd):
     }
     defaults = {k: v for k, v in settings.items()}
 
-    STOP, CONTINUE = True, False
-
     secret_code = None
     possible_codes = None
     guesses = 0
+    board = []
+    game_over = False
+    cracked = False
 
     def emptyline(self) -> bool:
         return False
@@ -128,6 +135,9 @@ class SuperHirn(Cmd):
             self.possible_codes = None
             self.secret_code = None
             self.guesses = 0
+            self.board.clear()
+            self.game_over = False
+            self.cracked = False
             print('+ Session ended. Mastermind set to defaults.')
         return self.CONTINUE
 
@@ -137,6 +147,8 @@ class SuperHirn(Cmd):
             if self.session_mode == 'codemaker':
                 print(f'+ Guesses so far: {self.guesses}')
                 print(f'+ Remaining guesses: {self.settings["limit"] - self.guesses}')
+                if self.game_over:
+                    self.reveal()
         else:
             print('+ Session not running.')
 
@@ -152,21 +164,101 @@ class SuperHirn(Cmd):
     def show_all(self) -> None:
         self.show_settings()
         self.show_session()
+        
+    def do_board(self, arg: str) -> bool:
+        """Show the current game board."""
+        code_width = 1 + self.settings['pins'] * 2
+        print("  ." + "-" * (14 + code_width) + ".")
+        heading = 'c o d e'.center(code_width)
+        print(f'  | pos |{heading}| score |')
+        print('  |' + "-" * (14 + code_width) + '|') 
+        for n in range(self.settings['limit']):
+            if n < len(self.board):
+                num = self.board[n][0]
+                code = ' '.join(
+                    [str(self.board[n][1][d]) for d in range(self.settings['pins'])]
+                ).center(code_width)
+                score = self.board[n][2][:self.settings['pins']]
+                print(f'  | {num:3} |{code}| {score:5} |')
+            else:
+                print('  |     |' + ' ' * code_width + '|       |')
+        print("  '" + "-" * (14 + code_width) + "'") 
+        return self.CONTINUE
+        
+    def do_guess(self, arg: str) -> bool:
+        """Enter guess as a codebreaker in a codemaker session: "guess 1 2 3 4 [5]"."""
+        args = []
+        try:
+            if self.session_mode != 'codemaker':
+                raise AttributeError('not in session')
+            if self.guesses >= self.settings['limit']:
+                raise AttributeError('too many guesses')
+            if self.cracked:
+                raise AttributeError('already cracked')
+            args = [int(a) for a in arg.split()]
+            if len(args) != self.settings['pins']:
+                raise ValueError('arg count')
+            if any([a < 0 or a >= self.settings['colors'] for a in args]):
+                raise ValueError('unknown color')
+            if not self.settings['repeat'] and len(args) != len(set(args)):
+                raise ValueError('repeats not allowed')
+        except ValueError:
+            print(f'*** Please enter exactly {self.settings["pins"]} single digits separated by blanks.')
+            print(f'*** Color codes must be in the range 0 ... {self.settings["colors"] - 1}.')
+            print(f'*** Color codes may{" " if self.settings["repeat"] else " not "}be repeated.')
+        except AttributeError as e:
+            if str(e) == 'not in session':
+                print('+ You must be in a codemaker session for this to work.')
+            elif str(e) == 'already cracked':
+                self.reveal()
+            else:
+                print(f'+ Too many guesses. Secret code {self.secret_code} not broken. Game over.')
+        else:
+            self.guesses += 1
+            score = self.score(tuple(args), self.secret_code)
+            self.board.append((self.guesses, tuple(args), score))
+            if self.guesses >= self.settings['limit'] and score != '+' * self.settings['pins']:
+                self.game_over = True
+                self.reveal()
+            elif score == '+' * self.settings['pins']:
+                self.game_over = True
+                self.cracked = True
+                self.reveal()
+        return self.do_board(arg)
 
-    # noinspection PyUnusedLocal
+    def score(self, a: Tuple[int, ...], b: Tuple[int, ...]) -> str:
+        if len(a) != len(b):
+            raise ValueError('*** Can not compare iterables of different length.')
+        result = 'o' * sum((Counter(a) & Counter(b)).values())
+        for x, y in zip(a, b):
+            if x == y:
+                result = result[1:] + '+'
+        return result
+
+    def reveal(self) -> None:
+        if self.cracked:
+            print(f'+ Congratulations! The secret code {self.secret_code} has been cracked.')
+        else:
+            print(f'+ Game over. Secret code {self.secret_code} not broken.')
+        
     def do_codemaker(self, arg: str) -> bool:
         """Switches mastermind to codemaker mode. Play a game against the machine, as a codebreaker."""
+        if self.session_mode:
+            print(f'*** Already in a {self.session_mode} session.')
+            print('*** Reset first, then start a new session.')
+            return self.CONTINUE
         argc = len(arg.split())
         if argc:
             print(f'*** {self.lastcmd}: Expected single word but got {argc} additional item{"s" if argc > 1 else ""}.')
             return self.CONTINUE
-        print('+ Codemaker session started.')
         self.possible_codes = {
             True: lambda c, p: list(product(range(c), repeat=p)),
             False: lambda c, p: list(permutations(range(c), p)),
         }[self.settings['repeat']](self.settings['colors'], self.settings['pins'])
         self.secret_code = choice(self.possible_codes)
         self.session_mode = 'codemaker'
+        print(f'+ {self.session_mode.capitalize()} did '
+            f'choose one secret code out of {len(self.possible_codes)}.')
         return self.CONTINUE
 
 
